0221d58756965d6f49dd3760a525500a8251c7e0
Udit Malik <51827904+malikudit@users.noreply.github.com>
2020-06-09 11:12:14 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ import pytz
 import requests
 import math
 
-api_key = "1d9d7d65228c86b14fdeac626cc4a75b"
+api_key = "INSERT_API_KEY_HERE"
 api_url = ("http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}")
 
 def query_api(city):
